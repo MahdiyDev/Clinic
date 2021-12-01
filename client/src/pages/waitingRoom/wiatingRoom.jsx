82de3bee853a4445a473802ec63f980e971ae100
MahdiyDev <mahdiydev@gmail.com>
2021-12-01 14:26:03 +0500

    queue_change

--- client/src/pages/waitingRoom/wiatingRoom.jsx
+++ client/src/pages/waitingRoom/wiatingRoom.jsx
@@ -6,11 +6,12 @@ function WaitingRoom() {
     const [queue] = useQueue('get')
     const [user] = useUserAuth()
     const [title, setTitle] = useState('')
+    const [foundUser, setFoundUser] = useState([])
 
     useEffect(() => {
         if (queue != null && user != null && queue.length) {
             if (queue[0].customer_email === user.customer_email) {
-                setTitle('Sani galing keldi')
+                setTitle('Sizni navbatingiz keldi')
             } else {
                 queue.map(e => {
                     if (e.customer_email !== user.customer_email) {
@@ -20,6 +21,8 @@ function WaitingRoom() {
                     }
                 })
             }
+            const findUser = queue.filter((q) => q.customer_email === user.customer_email)
+            setFoundUser(findUser)
         } else {
             setTitle('Iltimos joy olish uchun bosh saxifaga o`ting')
         }
@@ -28,6 +31,23 @@ function WaitingRoom() {
     return (
         <>
             <h1>{title}</h1>
+            <ul>
+                {queue != null && queue.length && user ? queue.map((e, i) => {
+                    if (e.customer_email === user.customer_email) {
+                        return (
+                            <h2 key={i}>Siznig {e.field_name}dagi navbatingiz: {i+1}</h2>
+                        )
+                    }
+                }) : []}
+                {foundUser.length ? foundUser.map((e) => {
+                    return (
+                        <li key={e.queue_uid}>
+                            <h3>{e.field_name}</h3>
+                            <small>{e.clinic_name}</small>
+                        </li>
+                    )
+                }) : []}
+            </ul>
         </>
     )
 }
